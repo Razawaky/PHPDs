d7aa26b6b34d30ad7acac57ea09bf018648758ca
Razawaky <viniciusduarte564@gmail.com>
2023-05-18 12:13:32 -0300

    funções

--- function/exerc1.php
+++ function/exerc1.php
@@ -13,13 +13,13 @@ function Maiornota($nota1, $nota2, $nota3, $nota4) {
     $resultado = "";
 
     if($nota1 > $nota2 && $nota1 > $nota3 && $nota1 > $nota4) {
-        $r = " Sua maior nota é a Primeira";
+        $resultado = " Sua maior nota é a Primeira";
     } if ($nota2 > $nota1 && $nota2 > $nota3 && $nota2 > $nota4) {
-        $r = " Sua maior nota é a Segunda";
+        $resultado = " Sua maior nota é a Segunda";
     } if($nota3 > $nota1 && $nota3 > $nota2 && $nota3 > $nota4) {
-        $r = " Sua maior nota é a Terceira";
+        $resultado = " Sua maior nota é a Terceira";
     } if($nota4 > $nota1 && $nota4 > $nota2 && $nota4 > $nota3) {
-        $r = " Sua maior nota é a Quarta";
+        $resultado = " Sua maior nota é a Quarta";
     }
     return $resultado;
 }
@@ -28,13 +28,13 @@ function Menornota($nota1, $nota2, $nota3, $nota4) {
     $resultado = "";
 
     if($nota1 < $nota2 && $nota1 < $nota3 && $nota1 < $nota4) {
-        $r = " Sua menor nota foi a Primeira";
+        $resultado = " Sua menor nota foi a Primeira";
     } if ($nota2 < $nota1 && $nota2 < $nota3 && $nota2 < $nota4) {
-        $r = " Sua menor nota foi a Segunda";
+        $resultado = " Sua menor nota foi a Segunda";
     } if($nota3 < $nota1 && $nota3 < $nota2 && $nota3 < $nota4) {
-        $r = " Sua menor nota foi a Terceira";
+        $resultado = " Sua menor nota foi a Terceira";
     } if($nota4 < $nota1 && $nota4 < $nota2 && $nota4 < $nota3) {
-        $r = " Sua menor nota foi a Quarta";
+        $resultado = " Sua menor nota foi a Quarta";
     }
     return $resultado;
 }
